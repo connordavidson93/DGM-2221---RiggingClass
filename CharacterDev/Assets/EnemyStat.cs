57312793e5a386bcd50be24c50d7c32b39fbb00e
Unknown <connordavidson93@gmail.com>
2018-03-06 11:51:17 -0700

    Trying to fix broken repo connections...

---
+++ CharacterDev/Assets/EnemyStat.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy") ]
+public class EnemyStat : ScriptableObject {
+    public string enemyName;
+    public string description;
+    public string element;
+    public int health;
+    public int strength;
+}
